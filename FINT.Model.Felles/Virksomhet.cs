f729a4a6b7299e75f10742b1e57b068dbe7ff718
Erling Jahr <erling.jahr@gmail.com>
2023-02-14 11:48:30 +0100

    Version v3.13.0 (#44)
    Co-authored-by: oleanders <oleanders@users.noreply.github.com>

---
+++ FINT.Model.Felles/Virksomhet.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+
+
+using FINT.Model.Felles.Kompleksedatatyper;
+using FINT.Model.Felles.Basisklasser;
+
+namespace FINT.Model.Felles
+{
+	public class Virksomhet : Enhet {
+	
+		public Identifikator VirksomhetsId { get; set; }
+		
+	}
+}
