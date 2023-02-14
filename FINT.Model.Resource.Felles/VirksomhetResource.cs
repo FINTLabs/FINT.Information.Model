ad36ea91968f864bf9c15eec4e62ce67f84883c2
oleanders <oleanders@users.noreply.github.com>
2023-02-14 10:34:04 +0000

    Version v3.13.0

---
+++ FINT.Model.Resource.Felles/VirksomhetResource.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using FINT.Model.Resource;
+
+using FINT.Model.Felles.Kompleksedatatyper;
+using FINT.Model.Felles.Basisklasser;
+
+namespace FINT.Model.Felles
+{
+
+    public class VirksomhetResource : EnhetResource 
+    {
+
+    
+        public Identifikator VirksomhetsId { get; set; }
+        
+    }
+}
