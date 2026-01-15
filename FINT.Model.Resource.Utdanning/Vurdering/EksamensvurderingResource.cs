dca9a27ef6436d006d0c9c6e80567a2100556f18
oleanders <oleanders@users.noreply.github.com>
2026-01-15 10:22:56 +0000

    Version v4.0.0-rc-4

--- FINT.Model.Resource.Utdanning/Vurdering/EksamensvurderingResource.cs
+++ FINT.Model.Resource.Utdanning/Vurdering/EksamensvurderingResource.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using FINT.Model.Resource;
 
-using FINT.Model.Utdanning.Vurdering;
-
 namespace FINT.Model.Utdanning.Vurdering
 {
 
--- FINT.Model.Utdanning/Vurdering/Fravarsoversikt.cs
+++ FINT.Model.Utdanning/Vurdering/Fravarsoversikt.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 
 
-using FINT.Model.Utdanning.Vurdering;
 using FINT.Model.Felles.Kompleksedatatyper;
 
 namespace FINT.Model.Utdanning.Vurdering
